315de6a6a797b9164d714e7f94e98a6e9c5db692
xxx_di <xxx_di@qq.com>
2015-05-21 10:52:25 +0800

    Test project initialization

---
+++ Test/src/test/Haha.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Haha {
+
+	public static void hehe(){
+		System.out.println("hehe");
+	}
+}
---
+++ Test/src/test/Test.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Test {
+
+	public static void test(int i){
+		System.out.println("test");
+	}
+}
