d5b25af3a1d5ea9aa25c1227f09cd6aa8bac6894
lykoyfy <lyklove0810@sina.com>
2015-07-11 13:11:29 +0800

    Test

---
+++ Test/src/test/TestRename.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test {
+
+	public static void test(int i){
+		System.out.println("test");
+	}
+
+	public void xixi(int haha){
+		haha++;
+		int xixi = haha;
+	}
+
+	public void addNew(int xixi){
+		xixi++;
+	}
+}
