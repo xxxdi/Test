f639b99614a375a5c392fe3edd9a8b181794dea5
lykoyfy <lyklove0810@sina.com>
2015-07-11 13:02:43 +0800

    Test Git Change
    change file name

---
+++ Test/src/test/Change.java
@@ -0,0 +1,46 @@
+package test;
+
+public class Haha {
+
+	public static void hehe(){
+		System.out.println("hehe");
+		System.out.println();
+		System.out.println();
+		System.out.println("");
+		System.currentTimeMillis();
+		System.currentTimeMillis();
+	}
+
+	public void haha(){
+		System.currentTimeMillis();
+		System.currentTimeMillis();
+	}
+
+	public void xixi(){
+		System.out.println("test feature1");
+		System.currentTimeMillis();
+
+	}
+
+	public void testLog(){
+		System.out.println("222");
+	}
+
+	public void all(){
+		System.out.println("change one");
+		System.out.println("test");
+	}
+
+	public void addOne(){
+		int i = 0;
+	}
+
+	public void changeOneFile(){
+		int addFileName = 0;
+		double changFileName;
+	};
+
+	public void updateAfterFileNameChanged(){
+		System.out.println("yeye!");
+	}
+}
